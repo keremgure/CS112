773eb2e6b07329199ab25e7afee4e88b396a92e6
Kerem Gure(Desktop PC) <keremgure@hotmail.com.tr>
2018-10-09 22:21:31 +0300

    First Commit of the repository with HW1 solutions.

---
+++ HW1/src.hs
@@ -0,0 +1,71 @@
+import Data.List
+import Data.Maybe
+count :: String -> Char -> Int
+count [] c = 0
+count (x:xs) c | x == c = 1 + count xs c
+               | otherwise = count xs c
+
+nubS :: String -> String
+nubS "" = ""
+nubS str = nubb str []
+
+
+nubb :: String -> [Char] -> String
+nubb [] list2 = []
+nubb (x:xs) list2 | includes list2 x = nubb xs list2
+                  | otherwise = x : nubb xs (x:list2)
+
+
+includes :: [Char] -> Char -> Bool
+includes [] c = False
+includes (x:xs) c | x == c = True
+                  | otherwise = includes xs c
+
+
+lengths :: (Eq a) => [[a]] -> [Int]
+lengths [[]] = [0]
+lengths lists = map getLength lists
+
+getLength :: (Eq a) => [a] -> Int
+getLength [] = 0
+getLength (x:xs) = 1 + getLength xs
+
+
+takeOdds :: [Int] -> [Int]
+takeOdds [] = []
+takeOdds (x:xs) | (odd x) = x : takeOdds xs
+                | otherwise = takeOdds xs
+
+wholePart :: String -> String
+wholePart "" = ""
+wholePart (x:xs) | x /= '.' = x:wholePart xs
+                 | otherwise = wholePart ""
+
+fracPart :: String -> String
+fracPart "" = ""
+fracPart (x:xs) | x == '.' = frac (x:xs) True
+                | otherwise = fracPart xs
+
+
+frac :: String -> Bool -> String
+frac "" False = ""
+frac (x:xs) True | x == '.' = frac xs True
+frac (x:xs) True | otherwise = (x:xs)
+
+whole2Int :: String -> Int
+whole2Int "" = 0
+whole2Int str = let st = wholePart str
+                in read st :: Int
+
+
+letterIndex :: Char -> Int
+letterIndex c = let (y:ys) = ['A'..'Z']
+                in fromJust $elemIndex c (y:ys)
+
+
+convertIndex :: String -> Int
+convertIndex "" = 0
+convertIndex (x:xs) = let size = getLength (x:xs)
+                          alphSize = 26
+                          elementIndex = letterIndex x
+                        in elementIndex + (alphSize * (size -1)) + convertIndex xs
